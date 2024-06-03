509329471886ce53a3f81dea0ad37dfef1f27ac8
MariyaNem <120091219+MariyaNem@users.noreply.github.com>
2024-06-04 01:31:29 +0700

    done

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- <HomePage />
+  <BlogPage/>
+  <BlogDetailsPage /> -->
+  <ProjectPage/>
+</template>
+
+<script>
+
+
+
+// import HomePage from './pages/HomePage.vue';
+// import BlogPage from './pages/BlogPage.vue';
+// import BlogDetailsPage from './pages/BlogDetailsPage.vue';
+import ProjectPage from './pages/ProjectPage.vue';
+export default {
+
+  components: {
+    // HomePage, 
+    // BlogPage,
+    // BlogDetailsPage,
+    ProjectPage,
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Jost:ital,wght@0,100..900;1,100..900&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+}
+
+.center {
+  padding-left: calc(50% - 600px);
+  padding-right: calc(50% - 600px);
+}
+</style>
